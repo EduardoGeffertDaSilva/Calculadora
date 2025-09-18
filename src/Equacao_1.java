7bce32209f6859032bff1d3d0f202b768601db51
eduardo_silva173 <gefferteduardo@gmail.com>
2025-09-18 18:19:54 -0300

    Func.1°.Implemetada

--- src/Equacao_1.java
+++ src/Equacao_1.java
@@ -1,2 +1,22 @@
-public class Equacao_1 {
+public class Equacao_1 extends Calcular
+{
+
+    public int Equacao(int a , int b, int c)
+    {
+
+        int x ;
+
+        if(b < 0)
+        {
+            x = c + b ;
+            x = x / a;
+            return x;
+        }
+        else
+        {
+            x = c - b;
+            x = x / a;
+            return x;
+        }
+    }
 }
