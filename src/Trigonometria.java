5f270b2b453561353da091f0ba50c53749b9ac0d
eduardo_silva173 <gefferteduardo@gmail.com>
2025-09-18 21:03:46 -0300

    ':i''✨ Fit: Adciona Trigonometria
    Adciona trigonometria no codigo

--- src/Trigonometria.java
+++ src/Trigonometria.java
@@ -1,7 +1,34 @@
 public class Trigonometria extends Calcular
 {
 
+    public double Seno(double co, double hp)
+    {
+        double s;
 
+        s = co / hp;
 
+        return s;
+    }
+
+    public double Cosseno(double ca, double hp)
+    {
+
+        double c;
+
+        c = ca / hp;
+
+        return c;
+
+    }
+
+    public double Tangente(double co, double ca)
+    {
+        double t;
+
+        t = co / ca;
+
+        return t;
+
+    }
 
 }
